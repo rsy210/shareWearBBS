2d5817b77c5aa0dcace072bed88de58cd51743b7
RSY <626929968@qq.com>
2015-09-25 14:03:31 +0800

    as

--- web/js/image_load.js
+++ web/js/image_load.js
@@ -1,7 +1,7 @@
 window.onload=function(){
 	waterfall('image_page','image_box');
 	var dataInt = {"data":[{"src":'1.jpg'},{"src":'1.jpg'},{"src":'1.jpg'},{"src":'1.jpg'},{"src":'1.jpg'},{"src":'1.jpg'},{"src":'1.jpg'}]};
-	window.onscroll=function(){		
+/*	window.onscroll=function(){		
 		if(checkScrollSlide) {
 			//数据库渲染到当前页面尾部
 			var oParent = document.getElementById('image_page');
@@ -18,7 +18,7 @@ window.onload=function(){
 			}
 
 		} 
-	}
+	}*/
 var option=document.getElementById("option");
 var lis = option.childNodes;
 for (var i = 0; i < lis.length; i++) {
@@ -77,3 +77,40 @@ function bgchange(obj){
              obj.style.backgroundColor='';
          }
      }
+
+
+//获取id
+function getId(id){
+	return document.getElementById(id);
+}
+
+//自动居中，登陆浮层
+function autoCenter(object){
+	var bodyW=document.documentElement.clientWidth;
+	var bodyH=document.documentElement.clientHeight;
+
+	var objW = object.offsetWidth;
+	var objH = object.offsetHeight;
+
+	object.style.left = (bodyW - objW)/2+'px';
+	object.style.top = (bodyH - objH)/2 + 'px';
+
+}
+autoCenter(getId('dialog'));
+
+//自动全屏，遮罩全屏
+function fillToBody(object){
+	object.style.width=document.documentElement.clientWidth + 'px';
+	object.style.height=document.documentElement.clientHeight+ 'px';
+}
+
+function showDialog(){
+getId('dialog').style.display='block';
+getId('mask').style.display='block';
+autoCenter(getId('dialog'));
+fillToBody(getId('dialog'));
+}
+function hideDialog(){
+getId('dialog').style.display='none';
+getId('mask').style.display='none';
+}
