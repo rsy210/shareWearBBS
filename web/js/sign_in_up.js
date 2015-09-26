7b5f6e72bdbbe71cf970072dd2861ebd93380798
rsy <626929968@qq.com>
2015-09-26 22:02:51 +0800

    signup

--- web/js/sign_in_up.js
+++ web/js/sign_in_up.js
@@ -1,72 +1,21 @@
-window.onload=function(){
-	waterfall('image_page','image_box');
-	var dataInt = {"data":[{"src":'1.jpg'},{"src":'1.jpg'},{"src":'1.jpg'},{"src":'1.jpg'},{"src":'1.jpg'},{"src":'1.jpg'},{"src":'1.jpg'}]};
-/*	window.onscroll=function(){		
-		if(checkScrollSlide) {
-			//数据库渲染到当前页面尾部
-			var oParent = document.getElementById('image_page');
-			for (var i = 0; i < dataInt.data.length; i++) {
-				var oBox = document.createElement('div');
-				oBox.className='image_box';
-				oParent.appendChild(oBox);
-				var oPic = document.createElement('div');
-				oPic.className='pic_style';
-				oBox.appendChild(oPic);
-				var oImg = document.createElement('img');
-				oImg.src="images/"+dataInt.data[i].src;
-				oPic.appendChild(oImg);
-			}
 
-		} 
-	}*/
-var option=document.getElementById("option");
+window.onload=function(){
+	
+
+var option=document.getElementById('option');
+
 var lis = option.childNodes;
 for (var i = 0; i < lis.length; i++) {
 		bgchange(lis[i]);
 };
 
-
 }
 
-function waterfall(parent,box){
-	//将parent下所有class为box元素取出
-	var oParent = document.getElementById(parent);
-	var oBoxs=getByClass(oParent,box);
-	
+//获取id
+function getId(id){
+	return document.getElementById(id);
 }
 
-function getByClass(parent,className){
-	var boxArr = new Array(); //存储获取的c指定lass元素
-	var oElements = parent.getElementsByTagName('*');
-	for (var i = 0; i < oElements.length; i++) {
-		if(oElements[i].className == className){
-		boxArr.push(oElements[i]);
-	}
-	}
-	return boxArr;
-}
-
-function checkScrollSlide(){
-	//检测是否加载数据块
-
-	var oParent = document.getElementById('image_page');
-
-	var oBoxs = getByClass(oParent,'image_box');
-
-
-	var lastBoxH = oBoxs[oBoxs.length-1].offsetTop + Math.floor(oBoxs[oBoxs.length-1].offsetHeight/2);
-console.log(lastBoxH);
-
-    var  h=document.documentElement.scrollHeight
-   || document.body.scrollHeight;
-console.log(h);
-	var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-	var height = document.body.clientHeight || document.documentElement.clientHeight; 
-console.log(scrollTop+height);
-
-
-	return (h <= scrollTop+height) ? true :false;
-}
 
 
 function bgchange(obj){
@@ -79,10 +28,6 @@ function bgchange(obj){
      }
 
 
-//获取id
-function getId(id){
-	return document.getElementById(id);
-}
 
 //自动居中，登陆浮层
 function autoCenter(object){
